753f89d96769942611f90fd39962cb816d9e3256
Jazzy <jahanzeb.baber@hawaiianair.com>
2021-06-17 06:06:31 -0700

    checkpoint

---
+++ json-logger/src/main/java/org/mule/extension/jsonlogger/internal/Constants.java
@@ -0,0 +1,18 @@
+package org.mule.extension.jsonlogger.internal;
+
+class Constants {
+  public static final String SCOPE_ELAPSED = "scopeElapsed";
+  static final String APPLICATION = "application";
+  static final String CONTENT = "content";
+  static final String CORRELATION_ID = "correlationId";
+  static final String LOCATION_INFO = "locationInfo";
+  static final String ROOT_CONTAINER = "rootContainer";
+  static final String FILE_NAME = "fileName";
+  static final String LINE_NUMBER = "lineNumber";
+  static final String TIMSTAMP = "timestamp";
+  static final String THREAD_NAME = "threadName";
+  static final String MESSAGE = "message";
+  static final String PRIORITY = "priority";
+  static final String TRACE_POINT = "tracePoint";
+  static final String ELAPSED = "elapsed";
+}
---
+++ json-logger/src/main/java/org/mule/extension/jsonlogger/internal/JsonLoggerConfig.java
@@ -0,0 +1,8 @@
+package org.mule.extension.jsonlogger.internal;
+
+import org.mule.extension.jsonlogger.api.pojos.LoggerConfig;
+import org.mule.runtime.extension.api.annotation.Operations;
+
+@Operations(JsonLoggerOperations.class)
+public class JsonLoggerConfig extends LoggerConfig {
+}
